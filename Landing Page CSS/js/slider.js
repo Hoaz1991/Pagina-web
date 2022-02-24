bbc7e6037fe00c53f17ca171546d602689751b42
Hoaz1991 <haroldaguirre1991@hotmail.com>
2022-02-23 20:46:41 -0500

    pagina web html, css y js parte15

--- Landing Page CSS/js/slider.js
+++ Landing Page CSS/js/slider.js
@@ -15,8 +15,17 @@
 
 const changePosition = (add)=>{
     const currentTestimony = document.querySelector('.testimony__body--show').dataset.id;
+    value = Number(currentTestimony);
+    value+= add;
 
-    console.log(currentTestimony);
+    sliders[Number(currentTestimony)-1].classList.remove('testimony__body--show');
+    if(value === sliders.length+1 || value === 0){
+        value = value === 0 ? sliders.length :1;
+    }
+    sliders[value-1].classList.add('testimony__body--show');
+    
 }
+
+
 
 })();
