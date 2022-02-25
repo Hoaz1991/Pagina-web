2005397a2dfbdd2b27adeb2af3ae6e768250d378
Hoaz1991 <haroldaguirre1991@hotmail.com>
2022-02-25 14:47:02 -0500

    pagina web html, css y js parte18

--- Landing Page CSS/js/questions.js
+++ Landing Page CSS/js/questions.js
@@ -5,6 +5,11 @@
        question.addEventListener('click', ()=>{
            let height = 0;
            let answer = question.nextElementSibling;
+           let addPadding = question.parentElement.parentElement;
+
+           addPadding.classList.toggle('questions__padding--add');
+
+           question.children[0].classList.toggle('.question__arrow--rotate')
 
            if(answer.clientHeight === 0){
                height = answer.scrollHeight;
